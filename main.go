4757c99c68c22e64c8fc29c628fb0b3abcc75904
ibzsy <ibzsy@proton.me>
2024-12-01 22:42:26 +0000

    Refactored Repl

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/ibzsy/cardboard/repl"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+
+	"github.com/ibzsy/cardboard/object"
+	"github.com/ibzsy/cardboard/repl"
+)
 
 func main() {
-	// READ -> EVALUATE -> PRINT -> LOOP
-	repl.StartREPL()
+	var buffer bytes.Buffer
+	var env = object.CreateEnvironment()
+
+	for {
+		repl.StartREPL(&buffer, env)
+		// Create a Scanner to read each string (line)
+		scanner := bufio.NewScanner(bytes.NewReader(buffer.Bytes()))
+
+		// Iterate over each line in the buffer
+		for scanner.Scan() {
+			fmt.Println(scanner.Text()) // Print each string
+		}
+
+		// Check for any errors during scanning
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading buffer:", err)
+		}
+		buffer.Reset()
+	}
 }
--- repl/repl.go
+++ repl/repl.go
@@ -2,52 +2,46 @@ package repl
 
 import (
 	"bufio"
+	"bytes"
+	"os"
+	"strings"
+
 	"github.com/ibzsy/cardboard/eval"
 	"github.com/ibzsy/cardboard/lexer"
 	"github.com/ibzsy/cardboard/object"
 	"github.com/ibzsy/cardboard/parser"
-	"fmt"
-	"os"
-	"strings"
 )
 
-func StartREPL() {
+func StartREPL(buffer *bytes.Buffer, env *object.Environment) {
 	scanner := bufio.NewScanner(os.Stdin)
-	env := object.CreateEnvironment()
 
-	fmt.Println("Cardboard v1.0! type :q to quit REPL.")
+	scanner.Scan()
 
-	for {
-		fmt.Print(">>> ")
-		scanner.Scan()
+	input := strings.TrimSpace(scanner.Text())
 
-		input := strings.TrimSpace(scanner.Text())
-
-		if input == "" {
-			return
-		} else if input == ":q" {
-			fmt.Println("Ending REPL.")
-			os.Exit(0)
-		}
-
-		lex := lexer.CreateLexer(input)
-		parser := parser.CreateParser(lex)
-		program := parser.ParseCardBoard()
-
-		if checkParserErrors(parser) {
-			continue
-		}
-
-		evaluatedProgram := eval.Eval(program, env)
-		fmt.Println(evaluatedProgram.Inspect())
+	if input == "" {
+		return
+	} else if input == ":q" {
+		os.Exit(0)
 	}
+
+	lex := lexer.CreateLexer(input)
+	parser := parser.CreateParser(lex)
+	program := parser.ParseCardBoard()
+
+	if checkParserErrors(parser, buffer) {
+		return
+	}
+
+	evaluatedProgram := eval.Eval(program, env)
+	buffer.WriteString(evaluatedProgram.Inspect())
 }
 
-func checkParserErrors(p *parser.Parser) bool {
+func checkParserErrors(p *parser.Parser, buffer *bytes.Buffer) bool {
 	errs := p.GetErrors()
 	if len(errs) > 0 {
 		for _, err := range errs {
-			fmt.Println(err)
+			buffer.WriteString(err)
 		}
 		return true
 	}
